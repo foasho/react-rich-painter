7167b429dca97e1d736888b747028885c3dd53d1
shoosaka <foa.sho0111@outlook.jp>
2025-12-02 08:32:26 +0900

    修正

--- showcase/handwritten-diary/next.config.ts
+++ showcase/handwritten-diary/next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  transpilePackages: ["react-rich-painter"],
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        react: path.resolve("./node_modules/react"),
+        "react-dom": path.resolve("./node_modules/react-dom"),
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
