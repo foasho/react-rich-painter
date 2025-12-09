5a4fec6b5531cf4b9af24549a8260a67c7ae66e8
shoosaka <foa.sho0111@outlook.jp>
2025-12-09 22:33:16 +0900

    手動保存時の自動適応

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,7 @@ import { ReactRichPainter } from "./lib";
 function App() {
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
-      <ReactRichPainter
-        width={800}
-        height={600}
-        preset="notebook"
-      />
+      <ReactRichPainter width={800} height={600} preset="notebook" />
     </div>
   );
 }
